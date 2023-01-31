2bc92314747679ed237b44b4eaa06f15883bcc9c
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 01:30:49 +0300

    es-20

---
+++ es-20/exercise.js
@@ -0,0 +1,9 @@
+let primitive = "Hello";
+
+if (typeof primitive === "string") {
+  console.log("string");
+} else if (typeof primitive === "number") {
+  console.log("number");
+} else if (typeof primitive === "boolean") {
+  console.log("boolean");
+}
