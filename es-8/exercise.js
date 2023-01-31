2d24170dda7af0091d391fca76ebc56cff7befbe
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 00:56:28 +0300

    es-8

---
+++ es-8/exercise.js
@@ -0,0 +1,11 @@
+let myVar = true;
+console.log(myVar, typeof myVar);
+
+myVar = Number(myVar);
+console.log(myVar, typeof myVar);
+
+myVar = String(myVar);
+console.log(myVar, typeof myVar);
+
+myVar = Boolean(myVar);
+console.log(myVar, typeof myVar);
