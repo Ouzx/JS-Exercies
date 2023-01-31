1017506cb609e267817293e555ca4c9b7f586bd0
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 01:13:03 +0300

    es-10

--- es-10/exercise.js
+++ es-10/exercise.js
@@ -1,5 +1,7 @@
 const firstYearCompleted = true;
 const yearsCompleted = 1;
 
-yearsCompleted == firstYearCompleted
-yearsCompleted === firstYearCompleted
+// double equal operator will convert the value to the same type and then compare
+// triple equal operator will not convert the value to the same type and then compare
+console.log(yearsCompleted == firstYearCompleted);
+console.log(yearsCompleted === firstYearCompleted);
