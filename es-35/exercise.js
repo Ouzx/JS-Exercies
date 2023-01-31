bef90ca52f291f20fa6e182b6bab2c9ba4d58824
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 01:57:07 +0300

    es-35

--- es-35/exercise.js
+++ es-35/exercise.js
@@ -1,9 +1,10 @@
+// the error is: ReferenceError: personName is not defined
+// to fix it, we need to declare the variable personName before the if statement
+// or we can remove the if statement
 function canPlay() {
   let sport = " Football";
 
-  if (true) {
-    let personName = "Cosimo";
-  }
+  let personName = "Cosimo";
 
   console.log(personName + sport);
 }
