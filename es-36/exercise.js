fda1b98f504c380103e6eededa68072c75da4bf8
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 02:01:09 +0300

    es-36

--- es-36/exercise.js
+++ es-36/exercise.js
@@ -1,8 +1,13 @@
+// sport is at the global scope, so it can be accessed from the any function
+let sport = " Football";
+
 function canPlay() {
-  let sport = " Football";
+  // personName defined at the function scope with a default value
+  // so it won't throw an error if the if statement is not executed
+  let personName = "Cosimo Default Value";
 
   if (true) {
-    let personName = "Cosimo";
+    personName = "Cosimo";
   }
 
   console.log(personName + sport);
