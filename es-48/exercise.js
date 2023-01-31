d8d6d3bef9e3b59ddc0512926ca28d929ffd0f3b
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 02:35:27 +0300

    es-48

--- es-48/exercise.js
+++ es-48/exercise.js
@@ -1,1 +1,22 @@
+const students = [
+  {
+    id: 1,
+    name: "John",
+    surname: "Doe",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "Jane",
+    surname: "Doe",
+    age: 21,
+  },
+  {
+    id: 3,
+    name: "John",
+    surname: "Smith",
+    age: 22,
+  },
+];
 
+console.log(students[0]);
