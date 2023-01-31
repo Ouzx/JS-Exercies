842b0f72904119e2b9755b8437582c2107a584ae
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 00:53:56 +0300

    es-7

---
+++ es-7/exercise.js
@@ -0,0 +1,9 @@
+let hello = Number("Ciao"); // number
+console.log(hello);
+// Ciao is not a number, so it will be converted to NaN
+
+let age = Boolean(18); // boolean
+console.log(age);
+
+let isGraduated = String(false); // string
+console.log(isGraduated);
