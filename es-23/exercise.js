e27f315a95982a68694b17b8d838158f92fa162a
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 01:37:23 +0300

    es-23

---
+++ es-23/exercise.js
@@ -0,0 +1,13 @@
+let roomType = "Basic Room";
+
+switch (roomType) {
+  case "Basic Room":
+    console.log("Prince for Basic Room --> 50€");
+    break;
+  case "Junior Suite Room":
+    console.log("Prince for Junior Suite Room --> 80€");
+    break;
+  case "Master Suite Room":
+    console.log("Prince for Master Suite Room --> 100€");
+    break;
+}
