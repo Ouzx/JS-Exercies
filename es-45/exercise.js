1ea4140c98d76d06bdd741efa7b091b8bf226373
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 02:30:01 +0300

    es-45

--- es-45/exercise.js
+++ es-45/exercise.js
@@ -1,1 +1,8 @@
+function Smartphone(brand, name, price) {
+  this.brand = brand;
+  this.name = name;
+  this.price = price;
+}
 
+const smartphone = new Smartphone("Apple", "SE", 1300);
+console.log(smartphone);
