210fa14de00ed4f4d1952007d8044bce87bab8a3
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 01:25:25 +0300

    es-18

--- es-18/exercise.js
+++ es-18/exercise.js
@@ -1,7 +1,7 @@
 let number = 7;
 
 if (number > 0) {
-
   console.log("The number is positive");
-  
 } else console.log("The number is negative");
+
+console.log(number > 0 ? "The number is positive" : "The number is negative");
