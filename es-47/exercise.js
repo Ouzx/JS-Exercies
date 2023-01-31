87eb656be6892640a07700ba2990a8958d2c4d5f
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 02:33:57 +0300

    es-47

--- es-47/exercise.js
+++ es-47/exercise.js
@@ -1,1 +1,9 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+console.log(numbers[0]);
+
+console.log(numbers[numbers.length - 1]);
+
+console.log(numbers.length);
+
+console.log(numbers[20]); // there is no 20th element in the array so it will return undefined
