feed3fbd9017b8cb99fb5faed10e4788bbb49ac4
Ouzx <oguzhankandakoglu@gmail.com>
2023-02-01 01:24:44 +0300

    es-17

---
+++ es-17/exercise.js
@@ -0,0 +1,10 @@
+const isSunnyDay = true;
+const isHomeWorkCompleted = false;
+
+let result;
+if (isSunnyDay && isHomeWorkCompleted) {
+  result = "Jesse can go out to play";
+} else {
+  result = "Jesse stays home";
+}
+console.log(result);
